1b7653c3dcb71d7a83c56e9075c2c15641d91d33
sultaale <impa1387@gmail.com>
2023-08-24 02:02:37 +0300

    Added "final" to the field employeeService

--- src/main/java/com/sultaale/controller/EmployeeController.java
+++ src/main/java/com/sultaale/controller/EmployeeController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Controller
 public class EmployeeController {
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     @Autowired
     public EmployeeController(EmployeeService employeeService) {
