f579871e4415fb6bbdd85f48992b93cd182b3a9a
sultaale <impa1387@gmail.com>
2023-08-24 01:29:46 +0300

    Added project

--- src/main/java/com/sultaale/dao/EmployeeDaoImpl.java
+++ src/main/java/com/sultaale/dao/EmployeeDaoImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao {
-    private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
     @Autowired
     public EmployeeDaoImpl(SessionFactory sessionFactory) {
