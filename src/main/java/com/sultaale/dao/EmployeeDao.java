7519a4c2183873d66a7a7ef6f0d7f45422bfe10d
Andrew Pertsev <andrewpertsev88@gmail.com>
2023-08-24 10:08:22 +0300

    Update EmployeeDao.java

--- src/main/java/com/sultaale/dao/EmployeeDao.java
+++ src/main/java/com/sultaale/dao/EmployeeDao.java
@@ -4,7 +4,7 @@ import com.sultaale.entity.Employee;
 import java.util.List;
 
 
-public interface EmployeeDao {
+public interface EmployeeDao { // АР: для CRUD нужен update  и delete методы
     public List<Employee> getEmployees();
     public void saveEmployee(Employee employee);
     public Employee getEmployee(int id);
